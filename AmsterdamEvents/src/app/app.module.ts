bef01da97e502a8d256ca275317e56f2198ae365
Nilava Kazal <nilava.kazal@hva.nl>
2021-09-09 10:42:36 +0200

    home & header without styling or content :D

---
+++ AmsterdamEvents/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/mainpage/header/header.component';
+import { HomeComponent } from './components/mainpage/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
